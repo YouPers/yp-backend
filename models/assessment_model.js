b113838ee3abefef012208d2cbba468c43e11f38
RBLU <reto.blunschi@creagy.ch>
2013-10-17 11:35:14 +0200

    refactored db initialization

--- models/assessment_model.js
+++ models/assessment_model.js
@@ -23,33 +23,13 @@ var question = new Schema({
 
 var AssessmentSchema = new Schema({
     name: {type: String, trim: true, required: true},
-    questionCats: [{ category: String,
-        questions: [question]}]
+    questionCats: [
+        { category: String,
+            questions: [question]}
+    ]
 });
 
 
 mongoose.model('Assessment', AssessmentSchema);
 
-
-// initialize Assessment DB if not initialized
-var Assessment = mongoose.model('Assessment');
-console.log("Assessment: checking whether Database initialization is needed...");
-Assessment.find().exec(function (err, assessments) {
-    if (err) {
-        throw err;
-    }
-    if (assessments.length === 0) {
-        console.log("Assessment: initializing assessment Database from File!");
-        var assessmentFromFile = require('../dbdata/assessment.json');
-        console.log(assessmentFromFile);
-        var newAss = new Assessment(assessmentFromFile);
-        console.log(newAss);
-        newAss.save(function(err) {
-            if (err) {
-                console.log(err.message);
-            }
-        });
-    } else {
-        console.log("Assessment: no initialization needed, as we already have entities (" + assessments.length + ")");
-    }
-});
+common.initializeDbFor(mongoose.model('Assessment'));
--- models/common.js
+++ models/common.js
@@ -19,7 +19,39 @@ module.exports = {
         topic: "workLifeBalance",
         activityPlannedStatus: "active old".split(' '),
 
+        // ActivityPlanEven enums
+        activityPlanEventStats: "open done".split(' '),
+
         // Assessment related enums
         questionType: "oneSided twoSided".split(' ')
+    },
+
+    initializeDbFor : function InitializeDbFor(Model) {
+        console.log(Model.modelName + ": checking whether Database initialization is needed...");
+        Model.find().exec(function (err, col) {
+            if (err) {
+                throw err;
+            }
+            if (col.length === 0) {
+                var filename = '../dbdata/' + Model.modelName + '.json';
+                var jsonFromFile = require(filename);
+                if (jsonFromFile) {
+                    console.log(Model.modelName + ": initializing assessment Database from File: " + filename);
+                    console.log(jsonFromFile);
+                    var newObj = new Model(jsonFromFile);
+                    console.log(newObj);
+                    newObj.save(function (err) {
+                        if (err) {
+                            console.log(err.message);
+                        }
+                    });
+                } else {
+                    console.log(Model.modelName + ": no initialization, because no file found: " + filename);
+                }
+            } else {
+                console.log(Model.modelName + ": no initialization needed, as we already have entities (" + col.length + ")");
+            }
+        });
+
     }
 };

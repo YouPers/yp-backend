8ec2b5042b0de3d244f0abe04a7430c586a61f66
RBLU <reto.blunschi@creagy.ch>
2013-10-18 13:27:47 +0200

    replace whole object by object ID in case client POSTed whole object where we only store ref

--- routes/generic.js
+++ routes/generic.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 module.exports = {
 
     getByIdFn: function (baseUrl, Model) {
@@ -49,6 +51,22 @@ module.exports = {
 
     postFn: function (baseUrl, Model) {
         return function (req, res, next) {
+            if (!req.body) {
+                return next(new Error('exptected JSON body in POST'));
+            }
+            // replace objects by ObjectId in case client sent whole object instead of reference only
+            // do this check only for properties of typ ObjectID
+            var schema = Model.schema;
+            _.filter(schema.paths, function (path) {
+                return (path.instance === 'ObjectID');
+            })
+                .forEach(function (myPath) {
+                    if ((myPath.path in req.body) && (!(typeof req.body[myPath.path] === 'string' || req.body[myPath.path] instanceof String))) {
+                        req.body[myPath.path] = req.body[myPath.path].id;
+                    }
+                });
+
+
             var newObj = new Model(req.body);
 
             newObj.save(function (err) {

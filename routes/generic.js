5d017918ec664ea8d6d00bf13ae1bc605447e9d6
RBLU <reto.blunschi@creagy.ch>
2013-10-18 15:05:39 +0200

    added sorting, limit, skip and better populate

--- routes/generic.js
+++ routes/generic.js
@@ -1,51 +1,163 @@
 var _ = require('lodash');
 
+////////////////////////////////////
+// helper functions
+
+var isF = function (o) {
+    for (var i = 1, l = arguments.length; i < l; i++) {
+        var v = arguments[i];
+        if (!(_.isFunction(v) || _.isFunction(o[v]))) {
+            return false;
+        }
+    }
+    return true;
+};
+
+function split(val, delim, ret) {
+    ret = ret || [];
+    delim = delim || ',';
+    if (!val) {
+        return ret;
+    }
+    if (Array.isArray(val)) {
+        val.forEach(function (v) {
+            split(v, delim, ret);
+        });
+    } else {
+        val.split(delim).forEach(function (v) {
+            ret.push(v);
+        });
+    }
+    return ret;
+}
+
+
+// query options
+
+var _addPopulation = function (queryparams, dbquery) {
+    // check whether our dbquery supports population
+    if (!(dbquery && dbquery.populate && isF(dbquery, 'populate'))) {
+        return dbquery;
+    }
+
+    var schema = dbquery && dbquery.model && dbquery.model.schema;
+
+    //handle array style populate.
+    if (Array.isArray(queryparams.populate) || typeof queryparams.populate === 'string') {
+        _populate(schema, dbquery, split(queryparams.populate));
+    } else {
+        //handle object style populate.
+        _.each(queryparams.populate, function (v, k) {
+            _populate(schema, dbquery, flatJoin(v));
+        });
+    }
+    delete queryparams.populate;
+    return dbquery;
+};
+
+//mongoose throws an exception if you try and populate an non ObjectID
+// this is suppose to guard against that. See if we can fix it.
+function _populate(schema, dbquery, paths) {
+    paths = Array.isArray(paths) ? paths : [paths];
+    for (var i = paths.length; i--;) {
+        var p = paths[i];
+        if (schema && schema.path) {
+            var ref = schema.path(p);
+            if (ref && (ref.instance && ref.instance === 'ObjectID' || ref.caster && ref.caster.instance === 'ObjectID')) {
+                dbquery.populate(p);
+            }
+        } else {
+            dbquery.populate(p);
+        }
+    }
+}
+
+function flatJoin(v) {
+    var splits = split(v), ret = [];
+    for (var i = splits.length; i--;) {
+        ret.push(v + '.' + splits[i]);
+    }
+    return ret;
+}
+
+
+var _addPagination = function (queryparams, dbquery) {
+    // pagination
+
+    if (!queryparams) {
+        return dbquery;
+    }
+    // check wheter our dbquery object supports skip and limit functions
+    if (!isF(dbquery, 'skip', 'limit')) {
+        return dbquery;
+    }
+    // max limit = 1000, default for limit (when called without value = 100)
+    var limit = Math.min(queryparams && queryparams.limit && 0 + queryparams.limit || 100, 1000);
+    var skip = queryparams && queryparams.skip || 0;
+    if (queryparams) {
+        // remove limit and skip because we have handled them
+        delete queryparams.limit;
+        delete queryparams.skip;
+    }
+    return dbquery.skip(skip).limit(limit);
+};
+
+
+var _addSort = function (queryparams, dbquery) {
+    if (!(queryparams && queryparams.sort && isF(dbquery, 'sort'))) {
+        return dbquery;
+    }
+    split(queryparams.sort).forEach(function (v, k) {
+        var parts = v.split(':', 2);
+        if (parts.length === 1) {
+            parts.push(1);
+        }
+        var _s = {};
+        _s[parts[0]] = parts[1];
+        dbquery.sort(_s);
+    });
+
+    delete queryparams.sort;
+    return dbquery;
+};
+
+
+var addQueryOptions = function (req, dbquery) {
+    dbquery = _addPagination(req.query, dbquery);
+    dbquery = _addPopulation(req.query, dbquery);
+    dbquery = _addSort(req.query, dbquery);
+    return dbquery;
+
+};
+
+/////////////////////////////////////
+// the generic route handlers
+
 module.exports = {
 
     getByIdFn: function (baseUrl, Model) {
         return function (req, res, next) {
-            if (req.params.populate) {
-                Model.findOne({_id: req.params.id}).populate(req.params.populate).exec(function (err, obj) {
+            addQueryOptions(req, Model.findOne({_id: req.params.id}))
+                .exec(function (err, obj) {
                     if (err) {
                         return next(err);
                     }
                     res.send(obj);
                     return next();
                 });
-            } else {
-                Model.findOne({_id: req.params.id}).exec(function (err, obj) {
-                    if (err) {
-                        return next(err);
-                    }
-                    res.send(obj);
-                    return next();
-                });
-            }
         };
-
     },
 
     getAllFn: function (baseUrl, Model) {
         return function (req, res, next) {
-            if (req.params.populate) {
-                console.log('getAllFn: Population with: ' + req.params.populate);
-                Model.find().populate(req.params.populate).exec(function (err, objList) {
-                    if (err) {
-                        return next(err);
-                    }
-                    res.send(objList);
-                    return next();
-                });
-
-            } else {
-                Model.find().exec(function (err, objList) {
-                    if (err) {
-                        return next(err);
-                    }
-                    res.send(objList);
-                    return next();
-                });
-            }
+            addQueryOptions(req, Model.find())
+                .exec(function (err, objList) {
+                if (err) {
+                    return next(err);
+                }
+                res.send(objList);
+                return next();
+            });
         };
     },
 

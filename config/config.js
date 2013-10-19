6d4a71715b3fa8962e049ccbd98571828f9d54f8
RBLU <reto.blunschi@creagy.ch>
2013-10-19 11:45:20 +0200

    added config for ci environment

--- config/config.js
+++ config/config.js
@@ -46,7 +46,24 @@ module.exports = {
         db_port: '47968',
         db_database: 'heroku_app18686651',
         db_user: 'yp-backend-db-user',
-        db_password: 'driveyourhealth'
+        db_password: 'driveyourhealth',
+        loggerOptions: {
+            name: 'Main',
+            streams: [
+                {
+                    stream: process.stdout,
+                    level: 'info'
+                },
+                {
+                    path: 'logs/server.log',
+                    level: 'debug'
+                }
+            ],
+            serializers: {
+                req: Logger.stdSerializers.req,
+                res: Logger.stdSerializers.res
+            }
+        }
     }, test: {
         root: require('path').normalize(__dirname + '/..'), app: {
             name: 'Nodejs Restify Mongoose Demo'

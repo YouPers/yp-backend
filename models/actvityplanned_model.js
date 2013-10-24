494ba1d88a84759c0a0cf325e9a9e206a112e1d2
RBLU <reto.blunschi@creagy.ch>
2013-10-24 13:02:45 +0200

    added recommendation logic, currently on the fly whenever needed

--- models/actvityplanned_model.js
+++ models/actvityplanned_model.js
@@ -53,43 +53,4 @@ var ActivityPlannedSchema =  common.newSchema({
 mongoose.model('ActivityPlanned', ActivityPlannedSchema);
 
 // initialize Activity DB if not initialized
-var ActivityPlanned = mongoose.model('ActivityPlanned');
-var Activity = mongoose.model('Activity');
-
-console.log("ActivityPlanned: checking whether Database initialization is needed...");
-ActivityPlanned.find().exec(function (err, allActivityPlanned) {
-    if (err) {
-        throw err;
-    }
-    if (allActivityPlanned.length === 0 ) {
-        console.log("ActivityPlanned: initializing from File!");
-
-        var filename = '../dbdata/ActivityPlanned.json';
-
-        try {
-            var plansFromFile = require(filename);
-            plansFromFile.forEach(function(plan) {
-
-                // match ActivtiyPlans to Activity by Id, because they are by number before...
-                Activity.findOne({number: plan.activity.number}, function(err, activity) {
-                    if (err) {
-                        console.log(err);
-                        throw err;
-                    }
-                    console.log(plan);
-                    console.log(activity);
-                    plan.activity = activity._id;
-                    var newPlan = new ActivityPlanned(plan);
-                    console.log(newPlan);
-                    newPlan.save();
-                });
-            });
-
-        } catch (Error) {
-            console.log(Error);
-        }
-
-    } else {
-        console.log("ActivityPlanned: no initialization needed, as we already have instances (" + allActivityPlanned.length + ")");
-    }
-});
+common.initializeDbFor(mongoose.model('ActivityPlanned'));

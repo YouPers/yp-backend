eb414a65a5d3d2ac157e169a05eacea4e51b87f5
RBLU <reto.blunschi@creagy.ch>
2013-11-05 10:03:43 +0100

    activity text to datamodel

--- models/activity_model.js
+++ models/activity_model.js
@@ -11,6 +11,7 @@ var mongoose = require('mongoose'),
 var ActivitySchema = common.newSchema({
     number: {type: String, trim: true, required: true},
     title: { type: String, trim: true, required: true },
+    text: {type: String, trim: true},
     source: { type: String, enum: common.enums.source},
     defaultfrequency: {type: String, enum: common.enums.activityPlannedFrequency},
     defaultexecutiontype: {type: String, enum: common.enums.executiontype},

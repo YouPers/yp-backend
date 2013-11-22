61d7e44028a445809d9c65c0f804c052c46609ab
RBLU <reto.blunschi@youpers.com>
2013-11-22 13:40:54 +0100

    fixing logging on CI

--- spec/icalendar_spec.js
+++ spec/icalendar_spec.js
@@ -7,8 +7,6 @@ describe('icalendar module', function() {
         var event = new ical.VEvent('cded25be-3d7a-45e2-b8fe-8d10c1f8e5a9');
         event.setSummary("Test calendar event");
         event.setDate(new Date(2011,11,1,17,0,0), new Date(2011,11,1,18,0,0));
-        console.log(event.toString());
-
         expect(event.toString().length).toBeGreaterThan(11);
 
     });

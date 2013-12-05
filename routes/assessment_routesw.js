abdae1a5b0733545e74efe033cb17639b81ab3e6
RBLU <reto.blunschi@youpers.com>
2013-12-05 20:21:22 +0100

    documented assessment API

---
+++ routes/assessment_routesw.js
@@ -0,0 +1,141 @@
+/**
+ * User Routes module
+ *    these routes require authenticated users
+ */
+
+var mongoose = require('mongoose'),
+    Assessment = mongoose.model('Assessment'),
+    AssessmentResult = mongoose.model('AssessmentResult'),
+    genericRoutes = require('./generic'),
+    passport = require('passport'),
+    handlers = require('../handlers/assessment_handlers.js');
+
+
+module.exports = function (swagger, config) {
+
+    var baseUrl = '/assessments',
+        baseUrlWithId = baseUrl + "/{id}";
+    var resultsUrl = baseUrl + '/{assId}/results';
+
+    swagger.addPost({
+        spec: {
+            description: "Operations about assessments and assessmentResults",
+            path: resultsUrl,
+            notes: "stores a new result",
+            summary: "stores a new result for the assessment with id assId",
+            method: "POST",
+            params: [swagger.pathParam("assId", "ID of the assessment for which to store a result", "string"),
+                swagger.bodyParam("assessmentResult", "The assessment result to store", "AssessmentResult")],
+            "responseMessages": [
+                {
+                    "code": 201,
+                    "message": "Created",
+                    "responseModel": "AssessmentResult"
+                },
+                {
+                    "code": 401,
+                    "message": "Unauthorized: client or user not authorized to call this method"
+                },
+                {
+                    "code": 204,
+                    "message": "assessment with this id not found"
+                }
+            ],
+            "nickname": "postAssessmentResult",
+            beforeCallbacks: [passport.authenticate('basic', { session: false })]
+        },
+        action: genericRoutes.postFn(resultsUrl, AssessmentResult)
+    });
+
+    swagger.addGet({
+            spec: {
+                description: "Operations about assessments and assessmentResults",
+                path: resultsUrl + '/newest',
+                notes: "always returns zero or one result, the newest result is the one with the newest timestamp",
+                summary: "returns a the newest assessmentResult for the current user and the assessment with id assId",
+                method: "GET",
+                params: [swagger.pathParam("assId", "ID of the assessment for which to store a result", "string")],
+                "responseClass": "AssessmentResult",
+                "errorResponses": [swagger.errors.invalid('assId'), swagger.errors.notFound("assessment")],
+                "nickname": "getNewestAssessmentResult",
+                beforeCallbacks: [passport.authenticate('basic', { session: false })]
+            },
+            action: handlers.getNewestResult(resultsUrl, AssessmentResult)
+        }
+    );
+
+    swagger.addGet({
+            spec: {
+                description: "Operations about assessments and assessmentResults",
+                path: resultsUrl,
+                notes: "returns an array of assessmentResults",
+                summary: "returns all assessmentResult for the current user and the assessment with id assId",
+                method: "GET",
+                params: [swagger.pathParam("assId", "ID of the assessment for which to store a result", "string")],
+                "responseClass": "AssessmentResult",
+                "errorResponses": [swagger.errors.invalid('assId'), swagger.errors.notFound("assessment")],
+                "nickname": "getAssessmentResults",
+                beforeCallbacks: [passport.authenticate('basic', { session: false })]
+            },
+            action: genericRoutes.getAllFn(resultsUrl, AssessmentResult)
+        }
+    );
+
+    swagger.addDelete({
+            spec: {
+                description: "Operations about assessments and assessmentResults",
+                path: resultsUrl,
+                notes: "can only delete the results for one specific assessement",
+                summary: "deletes all assessmentResults for the current user and the assessment with id assId",
+                method: "DELETE",
+                params: [swagger.pathParam("assId", "ID of the assessment for which to store a result", "string")],
+                "nickname": "deleteAssessmentResults",
+                beforeCallbacks: [passport.authenticate('basic', { session: false })]
+            },
+            action: genericRoutes.deleteAllFn(resultsUrl, AssessmentResult)
+        }
+    );
+
+
+    swagger.addGet({
+        spec: {
+            description: "Operations about assessments and assessmentResults",
+            path: baseUrl,
+            notes: "returns an array of all assessments",
+            summary: "returns all assessments in the system",
+            method: "GET",
+            "responseClass": "Assessment",
+            "nickname": "getAssessments"
+        },
+        action: genericRoutes.getAllFn(baseUrl, Assessment)
+    });
+
+    swagger.addGet({
+        spec: {
+            description: "Operations about assessments and assessmentResults",
+            path: baseUrlWithId,
+            notes: "returns the assessment by id, pass the Object Id as String ",
+            summary: "returns one specific assessment by id",
+            params: [swagger.pathParam("id", "ID of the assessment to fetch", "string")],
+            "errorResponses": [swagger.errors.invalid('id'), swagger.errors.notFound("assessment")],
+            method: "GET",
+            "responseClass": "Assessment",
+            "nickname": "getAssessment"
+        },
+        action: genericRoutes.getByIdFn(baseUrl, Assessment)
+
+    });
+
+    swagger.addDelete({
+        spec: {
+            description: "Operations about assessments and assessmentResults",
+            path: baseUrl,
+            notes: "Admin only! do not use if you don't know exactly what this does!",
+            summary: "deletes all assessments in the system",
+            method: "DELETE",
+            "nickname": "deleteAssessments"
+        },
+        action: genericRoutes.deleteAllFn(baseUrl, Assessment)
+    });
+
+};
--- routes/swaggerModels.js
+++ routes/swaggerModels.js
@@ -13,5 +13,81 @@ module.exports = {
             "role": { "type": "string", enum: ['individual', 'healthpromoter', 'admin']},
             "preferences": { type: "string", description: "users preferences, strucutre not documented yet"}
         }
+    },
+    "Question": {
+        "id": "Question",
+        required: ["id", "category", "title"],
+        properties: {
+            "id": {type: "string"},
+            "category": {type: "string"},
+            "title": {type: "string"},
+            "type": {type: "string"},
+            "mintext": {type: "string"},
+            "mintextexample": {type: "string"},
+            "mintextresult": {type: "string"},
+            "midtext": {type: "string"},
+            "midtextexample": {type: "string"},
+            "maxtext": {type: "string"},
+            "maxtextexample": {type: "string"},
+            "maxtextresult": {type: "string"},
+            "exptext": {type: "string"}
+        }
+    },
+    "QuestionCat": {
+        "id": "QuestionCat",
+        required: ["id", "category", "questions"],
+        properties: {
+            "id": {type: "string"},
+            "category": {type: "string"},
+            "questions": {
+                type: "array",
+                "items": {
+                    "$ref": "Question"
+                }
+            }
+
+        }
+    },
+    "Assessment": {
+        "id": "Assessment",
+        "required": ["id", "name", "questionCats"],
+        "properties": {
+            "id": {type: "string"},
+            "name": {type: "string"},
+            questionCats: {
+                type: "array",
+                items: {
+                    "$ref": "QuestionCat"
+                }
+            }
+        }
+    },
+    "AssessmentAnswer": {
+        "id": "AssessmentAnswer",
+        "required": ["question", "answer", "assessment"],
+        properties: {
+            assessment: {type: "string", description: "reference to the assessment this answer belongs to"},
+            question:{type: "string", description: "reference to the question this answer belongs to"},
+            answer: {type: "integer", description: "the actual answer to this question", minumum: "-100", maximum: "100"},
+            answered: {type: "boolean"}
+        }
+    },
+    "AssessmentResult": {
+        "id": "AssessmentResult",
+        "required": ["owner", "assessment", "answers"],
+        properties: {
+            owner: {type: "string", description: "reference to the user owning this Result"},
+            campaign: {type: "string", description: "reference to the campaign this result was entered in, used for statistics"},
+            assessment: {type: "string", description: "reference to the assessment this result belongs to"},
+            timestamp: {type: "date", description: "optional on POST, is filled by server when not submitted"},
+            answers: {
+                type: "array",
+                items: {
+                    "$ref": "AssessmentAnswer"
+                }
+            }
+        }
     }
+
+
 };

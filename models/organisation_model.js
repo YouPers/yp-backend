4d2c7fac3eb13a6bd53640e399ac65b34a2accd1
Boris <boris.wechselberger@youpers.com>
2014-02-03 15:35:43 +0100

    WL-56 / WL-225 model

---
+++ models/organisation_model.js
@@ -0,0 +1,26 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    common = require('./common'),
+    ObjectId = mongoose.Schema.ObjectId;
+
+/**
+ * Organization Schema
+ */
+var OrganizationSchema = common.newSchema({
+    name: { type: String, trim: true, required: true },
+    address: {type: String, trim: true},
+    sector: {type: String, trim: true},
+    nrOfEmployees: {type: String, trim: true},
+    administrators: {type: [{ type: ObjectId}]},
+    avatar: {type: String}
+});
+
+var model = mongoose.model('Organization', OrganizationSchema);
+
+module.exports = mongoose.model('Organization');
+
+
+
+common.initializeDbFor(model);

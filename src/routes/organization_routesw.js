5953f76e4553146bc01720758f9a4d12795dcd05
Boris <boris.wechselberger@youpers.com>
2014-03-05 11:01:30 +0100

    WL-198

--- src/routes/organization_routesw.js
+++ src/routes/organization_routesw.js
@@ -94,4 +94,50 @@ module.exports = function (swagger, config) {
         },
         action: generic.deleteByIdFn(baseUrl, Organization)
     });
+    swagger.addPost({
+        spec: {
+            description: "Invite an organization administrator",
+            path: baseUrlWithId + "/inviteOrganizationAdminEmail",
+            notes: "Posts a request for an invitation for one or more email-addresses",
+            summary: "Request an invitation for to become organization admin to be sent by the backend to the supplied email address(es)",
+            params: [
+                {
+                    paramType: "body",
+                    name: "email",
+                    description: "object with one property: 'email', an email address, or an array of adresses, or a separated String of emails (by ';, ')",
+                    dataType: "Object",
+                    required: true
+                }
+            ],
+            method: "POST",
+            "nickname": "postOrganizationAdminInvite",
+            accessLevel: 'al_orgadmin',
+            beforeCallbacks: []
+        },
+        action: organizationHandlers.postOrganizationAdminInviteFn
+    });
+
+    swagger.addPost({
+        spec: {
+            description: "Assign a organization admin",
+            path: baseUrlWithId + "/assignOrganizationAdmin",
+            notes: "Posts a request to add the current user as orgadmin for the organization: special endpoint that can be called without al_orgadmin but needs a token instead for auth",
+            summary: "With this endpoint a non-privileged user can assign himself to become organization admin when he has an invitation token.",
+            params: [
+                {
+                    paramType: "query",
+                    name: "token",
+                    description: "the authtoken the user has gotten with his invitation email",
+                    dataType: "String",
+                    required: true
+                }
+            ],
+            method: "POST",
+            "nickname": "assignOrganizationAdmin",
+            accessLevel: 'al_user',
+            beforeCallbacks: []
+        },
+        action: organizationHandlers.assignOrganizationAdminFn
+    });
+
 };

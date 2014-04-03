97d529065fb9455f4c82b6ba812f7c27dc38966b
RBLU <reto.blunschi@youpers.com>
2014-04-03 14:59:43 +0200

    do not store empty recWeights -> makes loading/saving of activities and scoring much faster

--- src/handlers/activity_handlers.js
+++ src/handlers/activity_handlers.js
@@ -6,6 +6,62 @@ var mongoose = require('mongoose'),
     error = require('../util/error'),
     handlerUtils = require('./handlerUtils');
 
+
+function _checkActivityWritePermission(sentActivity, user, cb) {
+
+
+    // if no author delivered set to authenticated user
+    if (!sentActivity.author) {
+        sentActivity.author = user.id;
+    }
+
+    if (_.contains(user.roles, auth.roles.productadmin)) {
+        // requesting user is a product admin
+        return cb();
+
+    } else if (_.contains(user.roles, auth.roles.orgadmin) || _.contains(user.roles, auth.roles.campaignlead)) {
+        // requesting user is a campaignlead or orgadmin
+        if (!sentActivity.campaign) {
+            return cb(new error.MissingParameterError('expected activity to have a campaign id', { required: 'campaign id' }));
+        }
+
+        Campaign.findById(sentActivity.campaign).exec(function (err, campaign) {
+            if (err) {
+                return error.handleError(err, cb);
+            }
+
+            if (!campaign) {
+                return cb(new error.ResourceNotFoundError('Campaign not found.', { id: sentActivity.campaign }));
+            }
+
+            // check whether the posting user is a campaignLead of the campaign
+            if (!_.contains(campaign.campaignLeads.toString(), user.id)) {
+                return cb(new error.NotAuthorizedError('The user is not a campaignlead of this campaign.', {
+                    userId: user.id,
+                    campaignId: campaign.id
+                }));
+            }
+            sentActivity.source = "campaign";
+            // everything is fine -->
+            return cb();
+        });
+
+
+    } else {
+        return cb(new error.NotAuthorizedError('POST of object only allowed if author is an org admin, campaign lead or productAdmin',
+            { user: user.id}));
+    }
+}
+
+function _removeEmptyRecWeights(sentActivity) {
+    if (_.isArray(sentActivity.recWeights)) {
+        _.remove(sentActivity.recWeights, function (recWeight) {
+            return (_.isNull(recWeight[1]) || _.isUndefined(recWeight[1] || recWeight[1] === 0)) &&
+                (_.isNull(recWeight[2]) || _.isUndefined(recWeight[2] || recWeight[1] === 0));
+        });
+    }
+}
+
 function postActivity(req, res, next) {
 
     if (!req.body) {
@@ -16,57 +72,17 @@ function postActivity(req, res, next) {
 
     handlerUtils.cleanPopulated(Activity, sentActivity);
 
+    _removeEmptyRecWeights(sentActivity);
+
     // check whether delivered author is the authenticated user
+    // only to be checkt for POST becuase in PUT it is allowed to update an activitiy that has been authored by
+    // somebody else.
     if (sentActivity.author && (req.user.id !== sentActivity.author)) {
         return next(new error.NotAuthorizedError({ author: sentActivity.author, user: req.user.id}));
     }
 
-    // if no author delivered set to authenticated user
-    if (!sentActivity.author) {
-        sentActivity.author = req.user.id;
-    }
 
-    function _checkPermission(cb) {
-
-        if (_.contains(req.user.roles, auth.roles.productadmin)) {
-            // requesting user is a product admin
-            return cb();
-
-        } else if (_.contains(req.user.roles, auth.roles.orgadmin) || _.contains(req.user.roles, auth.roles.campaignlead)) {
-            // requesting user is a campaignlead or orgadmin
-            if (!sentActivity.campaign) {
-                return cb(new error.MissingParameterError('expected activity to have a campaign id', { required: 'campaign id' }));
-            }
-
-            Campaign.findById(sentActivity.campaign).exec(function (err, campaign) {
-                if (err) {
-                    return error.handleError(err, cb);
-                }
-
-                if (!campaign) {
-                    return cb(new error.ResourceNotFoundError('Campaign not found.', { id: sentActivity.campaign }));
-                }
-
-                // check whether the posting user is a campaignLead of the campaign
-                if (!_.contains(campaign.campaignLeads.toString(), req.user.id)) {
-                    return cb(new error.NotAuthorizedError('The user is not a campaignlead of this campaign.', {
-                        userId: req.user.id,
-                        campaignId: campaign.id
-                    }));
-                }
-                sentActivity.source = "campaign";
-                // everything is fine -->
-                return cb();
-            });
-
-
-        } else {
-            return cb(new error.NotAuthorizedError('POST of object only allowed if author is an org admin, campaign lead or productAdmin',
-                { user: req.user.id}));
-        }
-    }
-
-    _checkPermission(function(err) {
+    _checkActivityWritePermission(sentActivity, req.user, function (err) {
         if (err) {
             return error.handleError(err, next);
         }
@@ -86,44 +102,32 @@ function postActivity(req, res, next) {
     });
 }
 
-function putActivity(req, res, next) {
 
-    req.log.trace({parsedReq: req}, 'Put updated Activity');
+function putActivity(req, res, next) {
 
     if (!req.body) {
         return next(new error.MissingParameterError({ required: 'activity object' }));
     }
 
     var sentActivity = req.body;
-    req.log.trace({body: sentActivity}, 'parsed req body');
+    handlerUtils.cleanPopulated(Activity, sentActivity);
 
-    // ref properties: replace objects by ObjectId in case client sent whole object instead of reference only
-    // do this check only for properties of type ObjectID
-    _.filter(Activity.schema.paths, function (path) {
-        return (path.instance === 'ObjectID');
-    })
-        .forEach(function (myPath) {
-            if ((myPath.path in sentActivity) && (!(typeof sentActivity[myPath.path] === 'string' || sentActivity[myPath.path] instanceof String))) {
-                sentActivity[myPath.path] = sentActivity[myPath.path].id;
-            }
-        });
+    _removeEmptyRecWeights(sentActivity);
 
-    // if no author delivered set to authenticated user
-    if (!sentActivity.author) {
-        sentActivity.author = req.user.id;
-    }
-
-    Activity.findById(req.params.id).exec(function (err, reloadedActivity) {
+    _checkActivityWritePermission(sentActivity, req.user, function (err) {
         if (err) {
             return error.handleError(err, next);
         }
-        if (!reloadedActivity) {
-            return next(new error.ResourceNotFoundError({ id: sentActivity.id}));
-        }
 
-        _.extend(reloadedActivity, sentActivity);
+        Activity.findById(req.params.id).exec(function (err, reloadedActivity) {
+            if (err) {
+                return error.handleError(err, next);
+            }
+            if (!reloadedActivity) {
+                return next(new error.ResourceNotFoundError({ id: sentActivity.id}));
+            }
 
-        if (_.contains(req.user.roles, auth.roles.productadmin)) {
+            _.extend(reloadedActivity, sentActivity);
 
             // try to save the new object
             reloadedActivity.save(function (err) {
@@ -134,52 +138,9 @@ function putActivity(req, res, next) {
                 res.send(200, reloadedActivity);
                 return next();
             });
-
-        } else if (!_.contains(req.user.roles, auth.roles.orgadmin) && !_.contains(req.user.roles, auth.roles.campaignlead)) {
-            // checks based on roles of requesting user
-            return next(new error.NotAuthorizedError('PUT of object only allowed if author is an org admin or a campaign lead', {
-                userId: req.user.id
-            }));
-        } else {
-            if (!reloadedActivity.campaign) {
-                return next(new error.MissingParameterError('expected activity to have a campaign id', { required: 'campaign id' }));
-            } else {
-
-                Campaign.findById(reloadedActivity.campaign).exec(function (err, campaign) {
-                    if (err) {
-                        return error.handleError(err, next);
-                    }
-                    if (!campaign) {
-                        return next(new error.ResourceNotFoundError('Campaign not found', { id: reloadedActivity.campaign }));
-                    }
-
-                    // check whether the posting user is a campaignLead of the campaign
-                    if (!_.contains(campaign.campaignLeads.toString(), req.user.id)) {
-                        return next(new error.NotAuthorizedError('The user is not a campaignlead of this campaign.', {
-                            userId: req.user.id,
-                            campaignId: campaign.id
-                        }));
-                    }
-
-                    // orgadmin's and campaignlead's can only manage campaign-specific activities
-                    reloadedActivity.source = "campaign";
-
-                    // try to save the new object
-                    reloadedActivity.save(function (err) {
-                        if (err) {
-                            return error.handleError(err, next);
-                        }
-
-                        res.header('location', '/api/v1/activities' + '/' + reloadedActivity._id);
-                        res.send(201, reloadedActivity);
-                        return next();
-                    });
-                });
-            }
-
-        }
-
+        });
     });
+
 }
 
 module.exports = {

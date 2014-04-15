558159dccb5b6885c1ca3f4a0823d8a8ce0dfc17
RBLU <reto.blunschi@youpers.com>
2014-04-15 21:28:19 +0200

    adjust tests to 5 offers per type

--- spec/activityOffers_spec.js
+++ spec/activityOffers_spec.js
@@ -51,7 +51,7 @@ consts.newUserInNewCampaignApi(
                     .expectStatus(200)
                     .afterJSON(function (recs) {
 
-                        expect(recs.length).toEqual(3);
+                        expect(recs.length).toEqual(5);
 
                         _.forEach(recs, function (rec) {
                             expect(rec.type[0]).toEqual('ypHealthCoach');
@@ -77,7 +77,7 @@ consts.newUserInNewCampaignApi(
                                     .auth(offerTestUser.username, "yp")
                                     .expectStatus(200)
                                     .afterJSON(function (recs) {
-                                        expect(recs.length).toEqual(3);
+                                        expect(recs.length).toEqual(5);
                                         _.forEach(recs, function (rec) {
                                             expect(rec.type[0]).toEqual('ypHealthCoach');
                                             expect(rec.activity.id).toBeDefined();
@@ -105,7 +105,7 @@ consts.newUserInNewCampaignApi(
                                                     .expectStatus(200)
                                                     .afterJSON(function (recs) {
 
-                                                        expect(recs.length).toEqual(4);
+                                                        expect(recs.length).toEqual(6);
                                                         expect(recs[0].type[0]).toEqual('campaignActivity');
                                                         expect(recs[0].prio[0]).toBeGreaterThan(recs[1].prio[0]);
 
@@ -138,7 +138,7 @@ consts.newUserInNewCampaignApi(
                                                                     .afterJSON(function (recs) {
 
 
-                                                                        expect(recs.length).toEqual(5);
+                                                                        expect(recs.length).toEqual(7);
                                                                         expect(recs[0].type).toContain('campaignActivityPlan'); // preferred type
                                                                         expect(recs[1].type).toContain('ypHealthCoach'); // preferred type
                                                                         expect(recs[2].type).toContain('campaignActivity'); // next by rank, personalInvitation not available
@@ -155,7 +155,7 @@ consts.newUserInNewCampaignApi(
                                                                                     .expectStatus(200)
                                                                                     .afterJSON(function (recs) {
 
-                                                                                        expect(recs.length).toEqual(4);
+                                                                                        expect(recs.length).toEqual(6);
 
                                                                                         expect(recs[0].type).toContain('campaignActivity'); // next by rank
                                                                                         expect(recs[0].type).not.toContain('campaignActivityPlan'); // joined plan should not show up anymore

47889dc646bec24835da7ecd6b4d9bb28369fd66
RBLU <reto.blunschi@youpers.com>
2014-04-29 07:30:49 +0200

    remove test that does not make sense anymore - a disabled

--- spec/sendSummaryMail_spec.js
+++ spec/sendSummaryMail_spec.js
@@ -19,15 +19,6 @@ describe('Send Summary Email', function() {
         }, {log: log, i18n: require('i18next')});
     });
 
-    it('should NOT send an email for the given user', function(done) {
-        mailBatch.sendSummaryMail( '52a97f1650fca98c29000055', moment().subtract('d',1), moment().add('d',1), function(err) {
-            expect(err).toBeDefined();
-            expect(err.message).toBeDefined();
-            expect(err.message).toEqual('error.notEnabled.dailyUserMail');
-            return done();
-        }, {log: log, i18n: require('i18next')});
-    });
-
     afterEach(function() {
         mongoose.connection.close();
 //        email.closeConnection();

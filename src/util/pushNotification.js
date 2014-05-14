4a9415196ed3ba59c11c8b66fe57b78bbbc8f3b1
RBLU <reto.blunschi@youpers.com>
2014-05-14 15:47:57 +0200

    YM-5 first implementation of Push Notification for iOS

--- src/util/pushNotification.js
+++ src/util/pushNotification.js
@@ -2,10 +2,36 @@ var apn = require('apn');
 
 var options = {
     cert: '../certs/yp_balance_cert.pem',
-    key: '../certs/yp_balance_key.pem'
+    key: '../certs/yp_balance_key_noenc.pem',
+    passphrase: ''
+
 };
 
-var apnConnection = new apn.Connection(options);
+var service = new apn.Connection(options);
+
+service.on('connected', function() {
+    console.log("Connected");
+});
+
+service.on('transmitted', function(notification, device) {
+    console.log("Notification transmitted to:" + device.token.toString('hex'));
+});
+
+service.on('transmissionError', function(errCode, notification, device) {
+    console.error("Notification caused error: " + errCode + " for device ", device, notification);
+});
+
+service.on('timeout', function () {
+    console.log("Connection Timeout");
+});
+
+service.on('disconnected', function() {
+    console.log("Disconnected from APNS");
+});
+
+service.on('socketError', console.error);
+
+
 
 var sendNotificationFn = function(device, message) {
 
@@ -20,7 +46,7 @@ var sendNotificationFn = function(device, message) {
     note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
     note.payload = {'messageFrom': 'Caroline'};
 
-    apnConnection.pushNotification(note, myDevice);
+    service.pushNotification(note, myDevice);
 
 };
 

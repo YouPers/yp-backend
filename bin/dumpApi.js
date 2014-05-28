f35b11619ef6e000eb86f871d2355883ed9af613
RBLU <reto.blunschi@youpers.com>
2014-05-28 09:28:39 +0200

    script to dump the api-docs into files

---
+++ bin/dumpApi.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var _ = require('lodash');
+var fs = require('fs');
+var Client = require('node-rest-client').Client;
+
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+});
+
+var targetDir = '/tmp/apidocs/';
+var baseUrl = 'http://localhost:8000';
+var url = baseUrl + '/api-docs';
+
+var client = new Client();
+
+client.get(url, function(data, response) {
+    var parsedData =  JSON.parse(data);
+
+    _.forEach(parsedData.apis, function(api) {
+
+
+        client.get(baseUrl + api.path, function(apidata, response) {
+
+            var resourceName = api.path.split('/')[2];
+            console.log(resourceName);
+
+            fs.writeFile(targetDir + resourceName + '.json', JSON.stringify(apidata), function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log("The file was saved!");
+                }
+            });
+        });
+
+
+    });
+});

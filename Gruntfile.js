b077c098ba3c022fdf6fff769e012df22f32642b
RBLU <reto.blunschi@youpers.com>
2014-06-17 10:59:36 +0200

    WL-914 fixed focux performance issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,9 @@ module.exports = function (grunt) {
                     delay: 1,
                     env: {
                         PORT: '8000',
-                        TZ: 'UTC'
+                        TZ: 'UTC',
+                        NODE_TIME_ENABLED: 'TRUE',
+                        NODE_TIME_KEY: '9f2bf583430d5bdf2636153a901ec841cd6a51fa'
                     }
                 }
             }

f24e411347f065f18eedc899fb1ddf5da300123d
Boris <boris.wechselberger@youpers.com>
2014-10-01 14:46:27 +0200

    WL-1127 - removed healthCoach messages from backend

--- src/routes/healthcoach_route.js
+++ src/routes/healthcoach_route.js
@@ -6,39 +6,6 @@ var coachHandlers = require('./../handlers/coach_handlers.js'),
 
 module.exports = function (swagger) {
 
-    var baseUrl = '/coachmessages';
-
-    swagger.addOperation({
-            spec: {
-                description: "get current HealthCoach messages for this user ",
-                path: baseUrl,
-                summary: "get currently suitable health coach messages for this user ",
-                params: [
-                    {
-                        "name": "uistate",
-                        "description": 'the current uistate of the user',
-                        "dataType": 'string',
-                        "required": true,
-                        "allowMultiple": false,
-                        "paramType": "query"
-                    },
-                    {
-                        "name": "debug",
-                        "description": 'returns the facts object that has been used to calculate the messages as last entry of the result array',
-                        "dataType": 'boolean',
-                        "required": false,
-                        "allowMultiple": false,
-                        "paramType": "query"
-                    }
-                ],
-                method: "GET",
-                responseClass: "Array[string]",
-                "nickname": "coachMessagesGet",
-                accessLevel: 'al_all'},
-            action: coachHandlers.getCoachMessagesFn
-        }
-    );
-
     swagger.addOperation({
         spec: {
             description: "Operations about ActivityOffers",

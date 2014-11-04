c35eaf33deccfa630cb9e7b390c3efcf9bb2f33f
RBLU <reto.blunschi@youpers.com>
2014-11-04 17:28:20 +0100

    test fixes

--- spec/invitation_spec.js
+++ spec/invitation_spec.js
@@ -69,7 +69,7 @@ consts.newUserInNewCampaignApi(
                                     .afterJSON(function (invitation) {
 
                                         expect(invitation.refDocs.length).toEqual(0);
-                                        expect(invitation.event).toEqual(newPlan.id);
+                                        expect(invitation.event.id).toEqual(newPlan.id);
 
                                     })
                                     .toss();

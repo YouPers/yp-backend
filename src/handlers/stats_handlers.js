b3dd899258d28223b47124b4bb676ddbfdf7b4db
RBLU <reto.blunschi@youpers.com>
2014-12-07 14:55:19 +0100

    WL-1327

--- src/handlers/stats_handlers.js
+++ src/handlers/stats_handlers.js
@@ -10,7 +10,7 @@ var statsQueries = require('../stats/statsQueries'),
 
 function constructQuery(queryDef, options) {
     // put the modelName into options, so the transformers can access it
-    options.modelName = queryDef.modelName;
+    options.queryModelName = queryDef.modelName;
     var pipe = mongoose.model(queryDef.modelName).aggregate();
 
     if ((options.scopeType === 'owner') || (options.scopeType === 'campaign') || options.scopeType === 'topic') {

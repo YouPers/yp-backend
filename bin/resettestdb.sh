c981c577a2b8ab3f926e6975da8e93602046958e
RBLU <reto.blunschi@youpers.com>
2015-01-12 15:50:23 +0100

    WL-1535

--- bin/resettestdb.sh
+++ bin/resettestdb.sh
@@ -6,6 +6,7 @@ if [ -z "$DB" ]; then
     DB="test_database"
 fi
 
-echo "**** RESETTING MONGO DATABASE localhost:$DB to dbdata/testset"
+echo "**** RESETTING MONGO DATABASE localhost:$DB to dbdata/testset and dbdata/content"
 mongo $DB --eval "db.dropDatabase();"
 bin/mongoimportexport.sh -d dbdata/testset -l $DB
+bin/mongoimportexport.sh -d dbdata/content -l $DB

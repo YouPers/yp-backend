97708e8329a7c9da91e65d28d2c26facd1aa0f14
RBLU <reto.blunschi@youpers.com>
2015-01-28 14:40:36 +0100

    WL-1408

--- bin/backupActivities.sh
+++ bin/backupActivities.sh
@@ -5,5 +5,5 @@ echo keeping mongo version $OLD_VERSION to switch back after using 2.4.9
 /usr/local/bin/brew switch mongo 2.4.9
 
 DATE=$(date +"%Y%m%d%H%M")
-/usr/local/bin/mongoexport --host 10.111.1.92:27017 -u nodeDbAccess -p yp13%mongodb%insp -c ideas -d hc-uat --jsonArray > /Users/retoblunschi/Documents/backendBackup/activities_$DATE.json
+/usr/local/bin/mongoexport --host localhost:27017  -c ideas -d test_database --jsonArray > idea.json
 /usr/local/bin/brew switch mongo $OLD_VERSION

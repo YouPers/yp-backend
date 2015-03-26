4b61f2dd8b7506f3a2667415674c885b4a3917d6
RBLU <reto.blunschi@youpers.com>
2015-03-26 14:06:51 +0100

    added backup of assessments and assementquestions

--- bin/backupActivities.sh
+++ bin/backupActivities.sh
@@ -6,4 +6,6 @@ echo keeping mongo version $OLD_VERSION to switch back after using 2.4.9
 
 DATE=$(date +"%Y%m%d%H%M")
 /usr/local/bin/mongoexport --host 10.111.1.92:27017 -u nodeDbAccess -p yp13%mongodb%insp -c ideas -d hc-uat --jsonArray > /Users/retoblunschi/Documents/backendBackup/activities_$DATE.json
+/usr/local/bin/mongoexport --host 10.111.1.92:27017 -u nodeDbAccess -p yp13%mongodb%insp -c assessments -d hc-uat --jsonArray > /Users/retoblunschi/Documents/backendBackup/assessments_$DATE.json
+/usr/local/bin/mongoexport --host 10.111.1.92:27017 -u nodeDbAccess -p yp13%mongodb%insp -c assessmentquestions -d hc-uat --jsonArray > /Users/retoblunschi/Documents/backendBackup/assessmentquestions_$DATE.json
 /usr/local/bin/brew switch mongo $OLD_VERSION

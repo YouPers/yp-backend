22252817875bd84915abe87c4507154df37eb2a3
RBLU <reto.blunschi@youpers.com>
2015-03-31 08:07:17 +0200

    use 2.4.12 as fallback for exporting

--- bin/backupActivities.sh
+++ bin/backupActivities.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 OLD_VERSION=`/usr/local/bin/mongo --version | cut -b 24-`
-echo keeping mongo version $OLD_VERSION to switch back after using 2.4.9
-/usr/local/bin/brew switch mongo 2.4.9
+echo keeping mongo version $OLD_VERSION to switch back after using 2.4.12
+/usr/local/bin/brew switch mongo 2.4.12
 
 DATE=$(date +"%Y%m%d%H%M")
 /usr/local/bin/mongoexport --host 10.111.1.92:27017 -u nodeDbAccess -p yp13%mongodb%insp -c ideas -d hc-uat --jsonArray > /Users/retoblunschi/Documents/backendBackup/activities_$DATE.json

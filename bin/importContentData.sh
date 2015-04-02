4c6ea134975e0a88fb54a1157b9a48d444cf9947
RBLU <reto.blunschi@youpers.com>
2015-04-02 17:07:21 +0200

    added username and password option

--- bin/importContentData.sh
+++ bin/importContentData.sh
@@ -6,5 +6,18 @@ if [ -z "$DB" ]; then
     DB="test_database"
 fi
 
+ARGS="-l $DB"
+
+if [ -z "$2" ]
+then
+    ARGS="$ARGS"
+else
+    echo $2
+    echo $3
+    ARGS="-d dbdata/content -u $2 -p $3 $ARGS"
+    echo $ARGS
+fi
+
 echo "**** importing Content Collections into MONGO DATABASE localhost:$DB from dbdata/content"
-bin/mongoimportexport.sh -d dbdata/content -l $DB
+echo "bin/mongoimportexport.sh $ARGS"
+bin/mongoimportexport.sh $ARGS

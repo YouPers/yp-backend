2b93d58397eff37cdadf0aee36aeb1a062d90014
RBLU <reto.blunschi@youpers.com>
2015-04-17 11:22:23 +0200

    support username / password in resetscript, needed for all mongo instances where authentication is turned on (ci, uat, prod)

--- bin/resettestdb.sh
+++ bin/resettestdb.sh
@@ -6,7 +6,17 @@ if [ -z "$DB" ]; then
     DB="test_database"
 fi
 
+ARGS="-l $DB"
+
+if [ -z "$2" ]
+then
+    ARGS="$ARGS"
+else
+    ARGS="-u $2 -p $3 $ARGS"
+    USERARGS="-u $2 -p $3"
+fi
+
 echo "**** RESETTING MONGO DATABASE localhost:$DB to dbdata/testset and dbdata/content"
-mongo $DB --eval "db.dropDatabase();"
-bin/mongoimportexport.sh -d dbdata/testset -l $DB
-bin/mongoimportexport.sh -d dbdata/content -l $DB
+mongo $DB $USERARGS --eval "db.getCollectionNames().forEach(function(c) { if (c.indexOf('system.') == -1) db[c].remove({}); })"
+bin/mongoimportexport.sh -d dbdata/testset $ARGS
+bin/mongoimportexport.sh -d dbdata/content $ARGS

f8a03ca064c5f37cc7470c9cbc1739acbffc587c
RBLU <reto.blunschi@youpers.com>
2015-05-08 14:38:45 +0200

    WL-2063

--- spec/sendSummaryMail_spec.js
+++ spec/sendSummaryMail_spec.js
@@ -1,37 +1,39 @@
-describe('Send Summary Email', function() {
+describe('Send Summary Email', function () {
     var config = require('../src/config/config');
     var ypbackendlib = require('ypbackendlib');
     var log = ypbackendlib.log(config);
     var mongoose = ypbackendlib.mongoose,
         moment = require('moment'),
-        mailBatch = require('../src/batches/dailySummaryMail'),
-        _ = require('lodash');
+        mailBatch = require('../src/batches/dailySummaryMail');
 
 
     beforeEach(function () {
-// initialize Database
-        var modelNames = require('../src/models').modelNames;
-
-        var schemaNames = ['user']; // schema names to be extended
-        var modelPath = __dirname + '/../src/models'; // path the schema extensions are located
-        var schemaExtensions = {};
-        _.forEach(schemaNames, function (name) {
-            schemaExtensions[name] = require(modelPath + '/' + name + '_schema');
-        });
-        ypbackendlib.initializeDb(config, modelNames, modelPath, undefined, undefined, schemaExtensions);
+        require('../src/util/database').initializeDb();
     });
 
-    it('should send an email for the given user', function(done) {
-        mailBatch.sendMail( '52a97f1650fca98c29000006', moment().subtract(1, 'd'), moment().add(1, 'd'), function(err) {
-            if (err) {
-                log.error(err);
-            }
-            expect(err).toBeUndefined();
-            return done();
-        }, {log: log, i18n: ypbackendlib.i18n.initialize()});
+    it('should send an email for the given user', function (done) {
+        mongoose.model('User')
+            .findById('52a97f1650fca98c29000006')
+            .select('+profile +campaign +username +email')
+            .populate('profile')
+            .populate('campaign')
+            .exec(function (err, user) {
+                if (err) {
+                    log.error(err);
+                    return done(err);
+                }
+                mailBatch.sendMail(user, moment().subtract(1, 'd'), moment().add(1, 'd'), function (err) {
+                    console.log('okokok2');
+                    if (err) {
+                        log.error(err);
+                    }
+                    expect(err).toBeUndefined();
+                    return done();
+                }, {log: log, i18n: ypbackendlib.i18n.initialize()});
+            });
     });
 
-    afterEach(function() {
+    afterEach(function () {
         mongoose.connection.close();
 //        email.closeConnection();
     });

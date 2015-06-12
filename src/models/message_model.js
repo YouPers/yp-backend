008c7f1a49994f91f52fef5bbd1181d022997f88
Boris <boris.wechselberger@youpers.com>
2015-06-12 15:40:23 +0200

    WL-2108

--- src/models/message_model.js
+++ src/models/message_model.js
@@ -3,6 +3,8 @@
  */
 var mongoose = require('ypbackendlib').mongoose,
     common = require('ypbackendlib').commmonModels,
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId,
     SocialInteraction = mongoose.model('SocialInteraction'),
     AbstractSocialInteractionSchema = require('./socialInteraction_schema');
 
@@ -13,7 +15,8 @@ var mongoose = require('ypbackendlib').mongoose,
 var MessageSchema = common.newSchema({
 
     language: { type: String, trim: true},
-    important: { type: Boolean, default: false }
+    important: { type: Boolean, default: false },
+    activity: {type: ObjectId, ref: 'Activity', required: false}
 
 }, undefined, AbstractSocialInteractionSchema);
 

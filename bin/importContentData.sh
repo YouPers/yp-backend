48909ee5d559350b0620d04f0c26881a46ff2ae1
RBLU <reto.blunschi@youpers.com>
2015-09-15 21:52:19 +0200

    add auth to script

--- bin/importContentData.sh
+++ bin/importContentData.sh
@@ -6,5 +6,15 @@ if [ -z "$DB" ]; then
     DB="test_database"
 fi
 
+ARGS="-l $DB"
+
+if [ -z "$2" ]
+then
+    ARGS="-d dbdata/content $ARGS"
+else
+    ARGS="-d dbdata/content -u $2 -p $3 $ARGS"
+fi
+
 echo "**** importing Content Collections into MONGO DATABASE localhost:$DB from dbdata/content"
-bin/mongoimportexport.sh -d dbdata/content -l $DB
+echo "bin/mongoimportexport.sh $ARGS"
+bin/mongoimportexport.sh $ARGS

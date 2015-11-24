e892745b5a07f63bb890fb6d86bab07b0a665f5b
RBLU <reto.blunschi@youpers.com>
2015-11-24 11:31:22 +0100

    add a predIo Event for socialinteraction dismissed

--- src/recommender/predio.js
+++ src/recommender/predio.js
@@ -2,7 +2,8 @@ var config = require('../config/config'),
     mongoose = require('ypbackendlib').mongoose,
     moment = require('moment'),
     request = require('request-json'),
-    log = require('ypbackendlib').log(config);
+    log = require('ypbackendlib').log(config),
+    SocialInteraction = require('../core/SocialInteraction');
 
 var predioClient = request.createClient(config.predio.URL, {
     qs: {
@@ -29,9 +30,21 @@ mongoose.model('User').on('add', function (user) {
             log.debug({result: result, body: body}, "Saved new user to predIo");
         }
     });
-
 });
 
+
+SocialInteraction.on('socialInteraction:dismissed', function (user, socialInteraction, socialInteractionDismissed) {
+    var data = {
+        event: "$set",
+        entityType: "user",
+        entityId: user.id,
+        eventTime: moment().toISOString()
+    };
+    log.debug({user: user.id, idea: socialInteraction.idea, reason: socialInteractionDismissed.reason, data: data.toObject()}, "adding an rating event to predio");
+
+
+    // TODO: add a rating event to predio
+});
 
 
 
